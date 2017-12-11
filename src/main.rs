1737988252fe08738a475a0b8cc57d0d0dd9f321
Jonny <6732997+jonny7@users.noreply.github.com>
2017-12-11 18:51:33 -0400

    final minigrep

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
